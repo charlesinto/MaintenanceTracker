c4f9c6b8cc82bb74770bdf36def8720891677462
charles <charles.onuorah@yahoo.com>
2018-06-19 19:23:52 +0100

    feat- get application  requests for admin user-#5764532

--- server/routes/Admin.js
+++ server/routes/Admin.js
@@ -7,8 +7,37 @@ import validateKey from '../services/validateKeys';
 import inputValidate from '../services/inputValidator';
 import executeQuery from '../services/queryExecutor';
 import bcrypt from 'bcrypt';
-
+const ADMIN_ROLE = 1
 let router = express.Router();
 
-
+router.get('/', verifyToken, (req,res)=>{
+    if(typeof req.token !== undefined && req.token !== ''){
+        let user = req.token;
+        if(!(user.role_id === ADMIN_ROLE)){
+            res.statusCode = 400;
+            res.setHeader('content-type', 'application/json');
+            return res.json({
+                message:'access denied'
+            })
+        }
+        let sql = 'SELECT * FROM BASE_REQUEST';
+        executeQuery(sql,'')
+        .then((result)=>{
+            res.statusCode = 200;
+            res.setHeader('content-type', 'application/json');
+            res.json({
+                message:'operation successful',
+                requests:result.rows
+            })
+        })
+        .catch((err)=>{
+            res.statusCode = 500;
+            res.setHeader('content-type', 'application/json');
+            res.json({
+                message:'operation failed',
+                err
+            })
+        })
+    }
+})
 export default router;
